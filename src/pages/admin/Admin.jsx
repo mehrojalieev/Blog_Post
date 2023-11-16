f9affb51fdea284556e9cfa8afc0e80ccf58e74c
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-16 08:42:22 +0500

    Created

---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from "../../layout/sidebar/Sidebar"
+import { Outlet } from "react-router-dom";
+
+const Admin = () => {
+  return (
+    <div>
+      Admin
+      
+    </div>
+  )
+}
+
+export default Admin
