cfb000487f22203a64fd1799321fc57b82c52c8b
mehrojalieev <aliyevmehrojbek0@gmail.com>
2023-11-20 12:37:26 +0500

    LOGOUT works successfully

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -18,6 +18,16 @@ const Admin = () => {
 
   const [openLogoutModal, setOpenLogoutModal] = useState(false)
 
+  // const [logoutFromProfile, setLogoutFromProfile] = useState()
+
+  const logoutFromProfile = () => {
+    localStorage.removeItem("user_id")
+    localStorage.removeItem("token")
+    setTimeout(() =>{
+      window.location.reload(true)
+    }, 1500)
+  }
+
   // USER ALL POSTS
   useEffect(() => {
     instanse(`/api/posts/`)
@@ -41,58 +51,58 @@ const Admin = () => {
   return (
     <>
       <div className='main__user-wrapper'>
-      <div className="about__user-container">
-        <div className="main__user-info">
-          <div>
-            <span><MdOutlineEmail /> Email:</span>
-            <p> {userData.email}</p>
-          </div>
-          <div>
-            <span><MdOutlineDriveFileRenameOutline /> Firstname:</span>
-            <p> {userData.firstname}</p>
-          </div>
-          <div>
-            <span><MdOutlineDriveFileRenameOutline />  Lastname:</span>
-            <p>{userData.lastname}</p>
-          </div>
-          <div>
-            <span><RiShieldUserLine /> Role:</span>
-            <p> {userData.role}</p>
-          </div>
+        <div className="about__user-container">
+          <div className="main__user-info">
+            <div>
+              <span> Email:</span>
+              <p> {userData.email} <MdOutlineEmail /></p>
+            </div>
+            <div>
+              <span> Firstname:</span>
+              <p> {userData.firstname} <MdOutlineDriveFileRenameOutline /></p>
+            </div>
+            <div>
+              <span>  Lastname:</span>
+              <p>{userData.lastname} <MdOutlineDriveFileRenameOutline /></p>
+            </div>
+            <div>
+              <span> Role:</span>
+              <p> {userData.role} <RiShieldUserLine /></p>
+            </div>
 
-        </div>
-        <div className="user-profile">
-          <p>{userData.fullname}</p>
-          <div className="main__user-logo">
-            <h3>{userData.firstname?.slice(0, 1)}</h3>
           </div>
-          <button onClick={() => setOpenLogoutModal(true)} className="main__user-logout"><TbLogout2 /> Log Out</button>
+          <div className="user-profile">
+            <p>{userData.fullname}</p>
+            <div className="main__user-logo">
+              <h3>{userData.firstname?.slice(0, 1)}</h3>
+            </div>
+            <button onClick={() => setOpenLogoutModal(true)} className="main__user-logout"><TbLogout2 /> Log Out</button>
+          </div>
+        </div>
+        <div className="main__posts-container">
+          {
+            userAllPosts.filter(myData => myData.author === user_id).map(userPost =>
+              <div key={userPost._id} className="main__user-card">
+                <h2>{userPost.title.slice(0, 28)}...</h2>
+                <img src={userPost.image} />
+                <p>{userPost.description.slice(0, 300)}</p>
+              </div>
+            )
+          }
+        </div>
+
+
+      </div>
+
+      {/* LOG OUT POP-UP */}
+
+      <div style={openLogoutModal ? { display: "block" } : { display: "none" }} className="logout__modal-wrapper">
+        <div style={openLogoutModal ? { display: "flex" } : { display: "none" }} className="logout-card">
+          <p>Do you want to logout ?</p>
+          <button onClick={logoutFromProfile} className="logout-btn">LOG OUT</button>
+          <button onClick={() => setOpenLogoutModal(false)} className="close__logout-modal"><IoMdClose /></button>
         </div>
       </div>
-      <div className="main__posts-container">
-        {
-          userAllPosts.filter(myData => myData.author === user_id).map(userPost =>
-            <div key={userPost._id} className="main__user-card">
-              <h2>{userPost.title.slice(0, 28)}...</h2>
-              <img src={userPost.image} />
-              <p>{userPost.description.slice(0, 300)}</p>
-            </div>
-          )
-        }
-      </div>
-
-      
-    </div>
-
-    {/* LOG OUT POP-UP */}
-
-        <div style={openLogoutModal ? {display: "block"} : {display: "none"}} className="logout__modal-wrapper">
-            <div style={openLogoutModal ? {display: "flex"} : {display: "none"}} className="logout-card">
-                <p>Do you want to logout ?</p>
-                <button className="logout-btn">LOG OUT</button>
-                <button onClick={() => setOpenLogoutModal(false)} className="close__logout-modal"><IoMdClose /></button>
-            </div>
-        </div>
     </>
   )
 }
